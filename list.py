0b0937a833758c8943c5508305d8e3ff4ea713aa
banipreetsingh16 <123930274+banipreetsingh16@users.noreply.github.com>
2023-02-02 21:55:51 -1200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,32 @@
+list = []                                                       #declaring an empty list 
+names= input("Enter your names here: ")                         #taking the input from the user
+list.append(names)                                              #appending the entered names into the empty list which we created
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+names= input("Enter your names here: ")
+list.append(names)
+
+print(f'Your names are: {list}')                                  #printing the list which we created which now stores all the names which the user entered
